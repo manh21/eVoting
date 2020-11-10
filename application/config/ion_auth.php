0b8e46d2c3f31756d51ca562645f2518435d2df3
Naufal Hakim <muhaqilanaufalhakim@gmail.com>
2020-11-10 15:47:08 +0700

    update config

--- application/config/ion_auth.php
+++ application/config/ion_auth.php
@@ -131,8 +131,8 @@ $config['argon2_admin_params']		= [
  | The library will fail for empty password or password size above 4096 bytes.
  | This is an arbitrary (long) value to protect against DOS attack.
  */
-$config['site_title']                 = "code.com";       // Site Title, example.com
-$config['admin_email']                = "muhaqilanaufalhakim@gmail.com"; // Admin Email, admin@example.com
+$config['site_title']                 = "example.com";       // Site Title, example.com
+$config['admin_email']                = "admin@example.com"; // Admin Email, admin@example.com
 $config['default_group']              = 'members';           // Default group, use name
 $config['admin_group']                = 'admin';             // Default administrators group, use name
 $config['identity']                   = 'email';             /* You can use any unique column in your table as identity column.
